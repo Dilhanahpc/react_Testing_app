3bb184faf6eddb60326b88b7c9890c1d6d2daa00
Chandupa Dilhana <chandupadilhan04@gmail.com>
2025-07-09 02:44:34 +0530

    button target text content change on double click // This commit modifies the Button component to change the button text to "OUCH! 🤕"

---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,5 @@
+function Button(){
+    const handleClick=(e)=> e.target.textContent="OUCH! 🤕";
+    return(<button onDoubleClick={(e)=>handleClick(e)}>click me 😁</button>);
+}
+export default Button;
