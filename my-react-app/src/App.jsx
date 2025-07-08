3c162067b154a8e414d5b3a6994c8a7b082f0c87
Chandupa Dilhana <chandupadilhan04@gmail.com>
2025-07-08 16:01:52 +0530

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,30 @@
+import List from './List';
+function App(){
+
+    const fruits = [
+        { id: 1, name: "apple", calories: 52 },
+        { id: 2, name: "orange", calories: 47 },
+        { id: 3, name: "banana", calories: 89 },
+        { id: 4, name: "kiwi", calories: 61 },
+        { id: 5, name: "mango", calories: 50 }
+    ];
+
+    const vegetables = [
+        { id: 6, name: "carrot", calories: 41 },
+        { id: 7, name: "broccoli", calories: 55 },
+        { id: 8, name: "spinach", calories: 23 },
+        { id: 9, name: "potato", calories: 77 },
+        { id: 10, name: "tomato", calories: 18 }
+    ];
+
+    
+    return (
+        <>
+            {fruits.length>0 && <List  category="Fruits" />} 
+            {vegetables.length>0 && <List items={vegetables} category="Vegetables" />}
+        </>
+    );
+}
+
+export default App;
+
---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,25 @@
+function List({ items = [], category = "Category" }){
+
+
+   // fruits.sort((a,b)=>a.name.localeCompare(b.name)); 
+   // fruits.sort((a,b)=>b.name.localeCompare(a.name));
+   // fruits.sort((a,b)=>a.calories-b.calories);
+    //fruits.sort((a,b)=>b.calories-a.calories);  
+    
+    //const highCalFruits=fruits.filter(fruit=>fruit.calories>=60);
+
+    const listItems = items.map(item =>
+                                            <li key={item.id}>
+                                            {item.name} - 
+                                            <b>{item.calories}</b> 
+                                            calories</li>)
+    return(<>
+    
+            <h3 className="list-category">{category}</h3>
+            <ul className="list-items">{listItems}</ul>
+            </>
+    );
+
+}
+// Removed deprecated List.defaultProps
+export default List;
